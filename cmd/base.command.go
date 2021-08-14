0f04e0a09a70f35e81d2d4351e6f958a4544db33
Justin Robertson <justinr@uprate.co.za>
2021-08-14 18:55:12 +0200

    extedned Command function to allow changing of CLI description

--- cmd/base.command.go
+++ cmd/base.command.go
@@ -6,11 +6,14 @@ import (
 	"service/service"
 )
 
-func Command(name string, handler func(cmd *cobra.Command, args []string)) *cobra.Command {
+func Command(name string, handler func(cmd *cobra.Command, args []string), description string) *cobra.Command {
+	if description == "" {
+		description = fmt.Sprintf("Request the running %s to execute the %s command", service.AppName, name)
+	}
 	return &cobra.Command{
 		Use:   fmt.Sprintf("command:%s", name),
-		Short: fmt.Sprintf("Request the running %s to execute the %s command", service.AppName, name),
-		Long:  fmt.Sprintf("Request the running %s to execute the %s command", service.AppName, name),
+		Short: description,
+		Long:  description,
 		Run:   handler,
 	}
 }
--- cmd/command.ping.go
+++ cmd/command.ping.go
@@ -9,5 +9,5 @@ func init() {
 	rootCmd.AddCommand(Command("ping", func(cmd *cobra.Command, args []string) {
 		service.InitializeDiary(test, level, rate)
 		service.Command("ping", natsUri, compileNatsOptions(), map[string]string{})
-	}))
+	}, "Ping the currently running service instance"))
 }
